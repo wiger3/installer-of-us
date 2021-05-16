890bc21ab8880c9a7a20117a4a316b575c4cc70a
wiger3 <74871505+wiger3@users.noreply.github.com>
2021-05-16 23:17:29 +0200

    Update 1.0.1

--- installer-of-us.py
+++ installer-of-us.py
@@ -7,9 +7,9 @@ init()
 
 print(f"\
 {Fore.YELLOW}=====================\n\
-Town of Us Instalator\n\
+Town of Us Installator\n\
 ====================={Style.RESET_ALL}\n\
-Wybierz wersje gry:\n\
+Select game version:\n\
 1. 2021.5.10s\n\
 2. 2021.4.14s\n\
 3. 2021.4.12s\n\
@@ -18,36 +18,36 @@ Wybierz wersje gry:\n\
 ")
 
 while True: # do ..
-    version = int(input("Wersja gry: ")) - 1
+    version = int(input("Game version: ")) - 1
     if 0 < version < 5: # .. while
         break
     else:
-        print(f"{Fore.RED}Niepoprawna wersja: {str(version + 1)}{Style.RESET_ALL}")
+        print(f"{Fore.RED}Invalid version: {str(version + 1)}{Style.RESET_ALL}")
 while True:
     susdir = input("Folder gry: ")
     if os.path.isfile(f"{susdir}\\Among Us.exe"):
         break
     else:
-        print(f"{Fore.RED}Folder nie zawiera Among Us: {susdir}{Style.RESET_ALL}")
+        print(f"{Fore.RED}Directory doesn't contain Among Us: {susdir}{Style.RESET_ALL}")
 
 modver = ["2.0.4", "2.0.2", "2.0.2", "2.0.0", "1.2.0"]
 link = "https://github.com/slushiegoose/Town-Of-Us/releases/download/v{0}/TownOfUs-v{0}.zip".format(modver[version])
 print()
 
-print(f"{Fore.GREEN}Pobieranie z: {link}{Style.RESET_ALL}")
+print(f"{Fore.GREEN}Downloading from: {link}{Style.RESET_ALL}")
 r = requests.get(link, allow_redirects=True)
 open('townofsus.zip', 'wb').write(r.content)
 
 moddir = f"{susdir}\\..\\Town of Us"
-print(f"{Fore.GREEN}Tworzenie folderu: {moddir}{Style.RESET_ALL}")
+print(f"{Fore.GREEN}Creating directory: {moddir}{Style.RESET_ALL}")
 shutil.copytree(susdir, moddir)
 
-print(f"{Fore.GREEN}Instalacja modu w folderze..{Style.RESET_ALL}")
+print(f"{Fore.GREEN}Installing mod in directory..{Style.RESET_ALL}")
 with zipfile.ZipFile("townofsus.zip", 'r') as suszip:
     suszip.extractall(moddir)
 
 os.remove("townofsus.zip")
 print()
-print(f"{Fore.YELLOW}Zainstalowano Town of Us w folderze: {moddir}!{Style.RESET_ALL}")
-input("Nacisnij Enter, aby otworzyc gre..")
+print(f"{Fore.YELLOW}Town of Us installed in folder: {moddir}!{Style.RESET_ALL}")
+input(f"{Fore.YELLOW}Press Enter, to open the game..{Style.RESET_ALL}")
 os.startfile(f"{moddir}\\Among Us.exe")
